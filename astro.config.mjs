56f365efca730b93f2ff70509109ee1eb9a6a122
Menachem Hornbacher <menachemhornbacher@gmail.com>
2025-09-16 21:02:31 -0400

    🚧 start on homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import expressiveCode from 'astro-expressive-code';
+import { expressiveCodeOptions } from './src/site.config'
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [expressiveCode(expressiveCodeOptions), icon()]
+});
